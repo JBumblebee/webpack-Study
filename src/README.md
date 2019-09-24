5d5fc5254455dae7ded7fca1f3bf619788dbdd4e
JBumblebee <532458665@qq.com>
2019-09-24 15:01:09 +0800

    init my project

---
+++ src/README.md
@@ -0,0 +1,5 @@
+#這是一個webpack學習項目
+
+##哈哈
+
+###作者bumblebee
---
+++ src/main.js
@@ -0,0 +1,22 @@
+// 入口文件
+// console.log('OK,项目框架可以使用了')
+
+//導入 vue
+import Vue from 'vue'
+
+//導入 MUI 的樣式
+import './lib/mui/css/mui.css'
+
+
+//按需導入 mint-ui 中的組件
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header)
+
+//导入 App 根组件
+import app from './App.vue'
+
+var vm = new Vue({
+
+    el: '#app',
+    render: c => c(app)
+})
